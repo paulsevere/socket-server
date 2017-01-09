9add8c53e0757e5f8123f8c9d04dcb1b2cf52979
paulsevere <pjo2110@columbia.edu>
2017-01-09 09:53:17 -0500

    initialized everything

--- socket.js
+++ socket.js
@@ -3,25 +3,3 @@ const io = require('socket.io')(1337)
 io.on('connection', socket => {
   console.log("socket connected")
 })
-// now that server is running
-// srv.listen(1337, '127.0.0.1', () => {
-//
-//   // make a request
-//   var options = {
-//     port: 1337,
-//     hostname: '127.0.0.1',
-//     headers: {
-//       'Connection': 'Upgrade',
-//       'Upgrade': 'websocket'
-//     }
-//   };
-//
-//   var req = http.request(options);
-//   req.end();
-//
-//   req.on('upgrade', (res, socket, upgradeHead) => {
-//     console.log('got upgraded!');
-//     socket.end();
-//     process.exit(0);
-//   });
-// });
